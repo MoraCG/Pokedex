69ca79bad3ec19911518988349feabc8558c0229
Mora Castelli G <mcastelligaraycochea314@alumno.unlam.edu.ar>
2024-05-02 11:49:20 -0300

    muestran los default values

--- formDeCreaciónEdición.php
+++ formDeCreaciónEdición.php
@@ -13,45 +13,66 @@
 </head>
 <body class="w3-light-gray">
 <?php
-include 'header.html';
-$editar = floatval($_GET['editar']);
-$accion = "";
-if ($editar == 0) {//si es 0 es para crear un pokémon
-    echo '<h2>Crear Pokémon</h2>';
-    $accion = 'action="crear.php"';
-} else {
-    echo '<h2>Editar Pokémon</h2>';
-    $accion = 'action="editar.php?editar=' . $editar . '"';
-}
-echo '<form ' . $accion . ' method="post" enctype="multipart/form-data" class="w3-margin">
-    <label for="fotoPokemon">Foto:</label>
-    <input type="file" name="fotoPokemon" id="fotoPokemon"><br>
-    <label for="nombre">Nombre del pokémon:</label>
-    <input type="text" name="nombre" id="nombre" value=""><br>
-    <label for="numeroPokemon">Numero:</label>
-    <input type="number" id="numeroPokemon" name="numeroPokemon"><br>
-    <label for="tipoPokemon">Tipo de pokémon</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Normal"> Normal</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Fuego"> Fuego</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Agua"> Agua</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Planta"> Planta</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Eléctrico"> Eléctrico</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Hielo"> Hielo</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Lucha"> Lucha</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Veneno"> Veneno</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Tierra"> Tierra</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Volador"> Volador</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Psíquico"> Psíquico</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Bicho"> Bicho</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Roca"> Roca</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Fantasma"> Fantasma</label><br>
-    <label><input type="checkbox" name="tipos[]" value="Dragón"> Dragón</label><br>
-    <label for="descripcion">Una breve descripción:</label><br>
-    <textarea name="descripcion" id="descripcion" cols="20" rows="3"></textarea><br>
-    <input type="submit" value="Enviar" name="submit">
-</form>';
+// Crear conexión
+$conn = new mysqli("localhost", "root", "", "test");
 
-include 'footer.html';
+// Verificar la conexión
+if ($conn->connect_error) {
+    die("La conexión falló: " . $conn->connect_error);
+}else{
+    include 'header.html';
+    $editar = floatval($_GET['editar']);
+    $accion = "";
+    $query1 = "SELECT T.nombre FROM pokemon P JOIN pokemon_tipo PoT ON P.id = PoT.pokemon_id JOIN Tipo T ON PoT.tipo_id = T.id WHERE P.id = $editar";
+    $resultado1 = mysqli_query($conn, $query1);
+    $tipo = mysqli_fetch_assoc($resultado1)['nombre'];
+    $query = "SELECT imagen, numero, nombre, descripcion FROM pokemon WHERE id = $editar";
+    $resultado = mysqli_query($conn, $query);
+
+    if ($editar == 0) {//si es 0 es para crear un pokémon
+        echo '<h2>Crear Pokémon</h2>';
+        $accion = 'action="crear.php"';
+    } else {
+        echo '<h2>Editar Pokémon</h2>';
+        $accion = 'action="editar.php?editar=' . $editar . '"';
+        $fila = mysqli_fetch_assoc($resultado);
+        $imagen = $fila['imagen'];
+        $numero = $fila['numero'];
+        $nombre = $fila['nombre'];
+        $descripcion = $fila['descripcion'];
+    }
+    echo '<form ' . $accion . ' method="post" enctype="multipart/form-data" class="w3-margin">
+        <label for="fotoPokemon">Foto:</label>
+        <input type="file" name="fotoPokemon" id="fotoPokemon"><br>
+        <img src="' . $imagen . '" alt="Imagen del Pokémon"><br>
+        <label for="nombre">Nombre del pokémon:</label>
+        <input type="text" name="nombre" id="nombre" value="' . $nombre . '"><br>
+        <label for="numeroPokemon">Numero:</label>
+        <input type="number" id="numeroPokemon" name="numeroPokemon" value="' . $numero . '"><br>
+        <label for="tipoPokemon">Tipo de pokémon</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Normal"' . ($tipo == "Normal" ? ' checked' : '') . '> Normal</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Fuego"' . ($tipo == "Fuego" ? ' checked' : '') . '> Fuego</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Agua"' . ($tipo == "Agua" ? ' checked' : '') . '> Agua</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Planta"' . ($tipo == "Planta" ? ' checked' : '') . '> Planta</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Eléctrico"' . ($tipo == "Eléctrico" ? ' checked' : '') . '> Eléctrico</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Hielo"' . ($tipo == "Hielo" ? ' checked' : '') . '> Hielo</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Lucha"' . ($tipo == "Lucha" ? ' checked' : '') . '> Lucha</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Veneno"' . ($tipo == "Veneno" ? ' checked' : '') . '> Veneno</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Tierra"' . ($tipo == "Tierra" ? ' checked' : '') . '> Tierra</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Volador"' . ($tipo == "Volador" ? ' checked' : '') . '> Volador</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Psíquico"' . ($tipo == "Psíquico" ? ' checked' : '') . '> Psíquico</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Bicho"' . ($tipo == "Bicho" ? ' checked' : '') . '> Bicho</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Roca"' . ($tipo == "Roca" ? ' checked' : '') . '> Roca</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Fantasma"' . ($tipo == "Fantasma" ? ' checked' : '') . '> Fantasma</label><br>
+        <label><input type="checkbox" name="tipos[]" value="Dragón"' . ($tipo == "Dragón" ? ' checked' : '') . '> Dragón</label><br>
+        <label for="descripcion">Una breve descripción:</label><br>
+        <textarea name="descripcion" id="descripcion" cols="20" rows="3">' . $descripcion . '</textarea><br>
+        <input type="submit" value="Enviar" name="submit">
+    </form>';
+
+    include 'footer.html';
+}
+
 ?>
 </body>
 </html>
