b74dc1c8a35682b076fe373b63cd0e3c6ade665c
Lucía Alarcón <lalarcon782@alumno.unlam.edu.ar>
2024-05-21 21:24:28 -0300

    if

--- Controller/LoginsController.php
+++ Controller/LoginsController.php
@@ -12,35 +12,6 @@ class LoginsController
         $this->presenter = $Presenter;
     }
 
-    /*
-            public function get()
-            {
-
-
-                if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["usuario"]) && isset($_POST["password"])) {
-                    $usuario = $_POST["usuario"];
-                    $password = $_POST["password"];
-
-                    // Realizar la validación del usuario y contraseña
-                    if ($this->model->LogInconsulta($usuario, $password)) {
-                        $_SESSION["usuario"] = $usuario;
-                    }
-                }
-                else {
-
-                    $_SESSION["error_login"] = "Usuario o contraseña incorrectos y presta atencion mora";
-                }
-
-
-
-                header("Location: index.php?controller=Pokedex&action=get");
-
-
-
-            }
-
-    */
-
     public function get()
     {
         if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST["usuario"]) && isset($_POST["password"])) {
@@ -50,6 +21,7 @@ class LoginsController
             // Realizar la validación del usuario y contraseña
             if ($this->model->LogInconsulta($usuario, $password)) {
                 $_SESSION["usuario"] = $usuario;
+                unset($_SESSION["error_login"]);
             } else {
                 $_SESSION["error_login"] = "Usuario o contraseña incorrectos. presta atencion mora";
             }
